a32d844ff70fac8e0dda083a1610c516db21b59b
Rimantas <belauzas@yahoo.com>
2019-12-12 20:38:22 +0200

    Funkcijos

--- js/main.js
+++ js/main.js
@@ -194,15 +194,15 @@ for ( let i=0; i<10; i++ ) {
 console.log('---------------');
 
 
-const nuo = -3;
-const iki = -5;
+const nuo1 = -3;
+const iki1 = -5;
 let suma = 0;
 
-for ( let i=nuo; i<=iki; i++ ) {
+for ( let i=nuo1; i<=iki1; i++ ) {
     suma = suma + i;
 }
 
-console.log('Suma intervale nuo '+nuo+' iki '+iki+' yra lygi '+suma);
+console.log('Suma intervale nuo '+nuo1+' iki '+iki1+' yra lygi '+suma);
 
 
 console.log('---------------');
@@ -215,3 +215,152 @@ for ( let i=tekstas.length-1; i>=0; i-- ) {
     atbulas = atbulas + tekstas[i];
     console.log(i, tekstas[i], atbulas);
 }
+
+
+function sumavimas( pirmas, antras ) {
+    return pirmas + antras;
+}
+
+console.log( sumavimas( 7, 5 ) );
+console.log( sumavimas( 2, 2, 8 ) );
+console.log( sumavimas( -2, 9 ) );
+console.log( sumavimas( -2 ) );
+
+function sveikinu( name ) {
+    return name + ' buk sviekas!';
+}
+
+console.log( sveikinu('Vardeni') );
+console.log( sveikinu('Jonai') );
+console.log( sveikinu('Maryte') );
+
+
+
+console.log('---------------');
+
+// 1) logiskas, bet pats neefektyviausias budas
+
+// const nuo = -6;
+// const iki = 6;
+// const daliklis = 5;
+// let kiekis = 0;
+
+// einame per skaiciu intervala (for ciklas)
+    // jeigu (if) tas skaicius dalinasi be liekanos
+        // tai kiekis+1
+
+// for ( let i=nuo; i<=iki; i++) {
+//     if ( i % daliklis === 0 ) {
+//         kiekis++;
+//     }
+// }
+
+
+// 2) logiskas, bet tik nezymiai efektyvesnis ir veikia tik jei intervalas prasideda su be liekanos besidalinanciu
+
+// const nuo = -6;
+// const iki = 6;
+// const daliklis = 5;
+// let kiekis = 0;
+
+// for ( let i=nuo; i<=iki; i=i+daliklis ) {
+//     kiekis++;
+// }
+
+
+// 3) pats efektyviausias
+
+// const nuo = 0;
+// const iki = 12;
+// const daliklis = 3;
+// let kiekis = 0;
+
+// // ( iki - nuo ) / daliklis + 1
+// if ( nuo >= 0 && iki >= 0 ) {
+//     const galas = Math.floor(iki / daliklis) + 1;
+//     const pradzia = Math.floor(nuo / daliklis) + 1;
+//     kiekis = galas - pradzia;
+// }
+
+// console.log('Skaiciu intervale tarp ' + nuo + ' ir ' + iki + ', besidalinanciu be liekanos is ' + daliklis + ' yra ' + kiekis + ' vienetai.');
+
+function dalikliuKiekis( nuo, iki, daliklis ) {
+    let kiekis = 0;
+
+    const galas = Math.floor(iki / daliklis) + 1;
+    const pradzia = Math.floor(nuo / daliklis) + 1;
+    kiekis = galas - pradzia;
+    if ( nuo % daliklis === 0 ) {
+        kiekis++;
+    }
+
+    const ats = 'Skaiciu intervale tarp ' + nuo + ' ir ' + iki + ', besidalinanciu be liekanos is ' + daliklis + ' yra ' + kiekis + ' vienetai.';
+
+    return ats;
+}
+
+console.log( dalikliuKiekis( 0, 12, 3 ) );
+console.log( dalikliuKiekis( 1, 12, 3 ) );
+console.log( dalikliuKiekis( 2, 12, 3 ) );
+console.log( dalikliuKiekis( 3, 12, 3 ) );
+
+console.log( dalikliuKiekis( -12, 0, 3 ) );
+console.log( dalikliuKiekis( -12, -1, 3 ) );
+console.log( dalikliuKiekis( -12, -2, 3 ) );
+console.log( dalikliuKiekis( -12, -3, 3 ) );
+
+console.log( dalikliuKiekis( 0, 11, 3 ) );
+console.log( dalikliuKiekis( 1, 11, 3 ) );
+console.log( dalikliuKiekis( 2, 11, 3 ) );
+console.log( dalikliuKiekis( 3, 11, 3 ) );
+
+console.log( dalikliuKiekis( -11, 0, 3 ) );
+console.log( dalikliuKiekis( -11, -1, 3 ) );
+console.log( dalikliuKiekis( -11, -2, 3 ) );
+console.log( dalikliuKiekis( -11, -3, 3 ) );
+
+console.log('*****************');
+console.log('tusciaFunkcija');
+console.log('*****************');
+
+function tusciaFunkcija() {
+    return false;
+}
+
+console.log( tusciaFunkcija() );
+
+console.log('*****************');
+console.log('daugyba');
+console.log('*****************');
+
+function daugyba( pirmas, antras ) {
+    if ( isFinite(pirmas) === false ||
+         typeof(pirmas) !== 'number' ) {
+        return 'ERROR: pirma reiksme ne normalus skaicius.'
+    }
+    if ( isFinite(antras) === false ||
+         typeof(antras) !== 'number' ) {
+        return 'ERROR: antra reiksme ne normalus skaicius.'
+    }
+
+    return pirmas * antras;
+}
+
+console.log( daugyba( 2, 2 ) );
+console.log( daugyba( 2, 3 ) );
+console.log( daugyba( 2, -3 ) );
+console.log( daugyba( -2, 3 ) );
+console.log( daugyba( -2, -3 ) );
+console.log( daugyba( 'asd', 'qwe' ) );
+console.log( daugyba( true, false ) );
+console.log( daugyba( [], 5 ) );
+console.log( daugyba( [], 'asd' ) );
+console.log( daugyba( 5, 'asd' ) );
+console.log( daugyba( [58], 5 ) );
+console.log( daugyba( ['58'], 5 ) );
+console.log( daugyba( ['58s'], 5 ) );
+console.log( daugyba( ['ASD'], 5 ) );
+console.log( daugyba( 5, null ) );
+console.log( daugyba( Infinity, 2 ) );
+console.log( daugyba( NaN, Infinity ) );
+console.log( daugyba( NaN, 8 ) );
